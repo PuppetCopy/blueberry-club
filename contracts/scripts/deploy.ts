0cf2508f0777d24c25e0ed57b55cce2cfb6b55a1
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2021-11-22 21:26:21 +0200

    hardhat boilerplate

---
+++ contracts/scripts/deploy.ts
@@ -0,0 +1,26 @@
+import { GBC__factory } from 'contracts'
+import { ethers } from "hardhat"
+
+// .env file (should be ignored from .gitignore)
+import dotEnv from 'dotenv'
+dotEnv.config()
+
+const main = async () => {
+  const signer = (await ethers.getSigners())[0]
+
+  console.log('Your wallet address:', signer.address)
+
+  const contractFactory = new GBC__factory(signer)
+
+  const gbcContract = await contractFactory.deploy('GMX Blueberry Club', 'GBC', 'ipfs://hash/')
+  await gbcContract.deployed()
+  console.log(`🚀 contract is deployed to ${gbcContract.address} 🚀`)
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
