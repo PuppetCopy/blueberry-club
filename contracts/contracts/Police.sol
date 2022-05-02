cc483e3ce76f8ea511bbd68b135461f76e705fd5
IrvingDevPro <irving.dev.pro@gmail.com>
2022-05-02 18:38:29 +0200

    💡  Addinf some comments

--- contracts/contracts/Police.sol
+++ contracts/contracts/Police.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.0;
 
 import {RolesAuthority, Authority} from "@rari-capital/solmate/src/auth/authorities/RolesAuthority.sol";
 
+/**
+ * @title Police
+ * @author IrvingDevPro
+ * @notice This contract manage user and roles permisions
+ * in all contracts which this contract setted as autority
+ */
 contract Police is RolesAuthority {
     constructor() RolesAuthority(msg.sender, Authority(address(0))) {}
 }
