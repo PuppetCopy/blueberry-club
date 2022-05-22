b67e04365610bc09aaa42a2ea41ca8950b408d43
IrvingDevPro <irving.dev.pro@gmail.com>
2022-05-22 19:16:13 +0200

    Improve

--- contracts/contracts/lab/Closet.sol
+++ contracts/contracts/lab/Closet.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import {GBCLab as IGBCLab} from "./GBCLab.sol";
+import { IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {GBCLab} from "./GBCLab.sol";
 import {Auth, Authority} from "@rari-capital/solmate/src/auth/Auth.sol";
+
+error NotOwner();
 
 /**
  * @title Closet
@@ -17,7 +19,7 @@ contract Closet is Auth {
     uint public constant SPECIAL_ATTRIBUTE = 8;
 
     IERC721 public GBC;
-    IGBCLab public GBCLab;
+    GBCLab public LAB;
 
     struct Items {
         uint background;
@@ -36,32 +38,31 @@ contract Closet is Auth {
         uint special
     );
 
-    constructor(address _gbc, address _gbcLab, address _owner, Authority _authority) Auth(_owner, _authority) {
-        GBC = IERC721(_gbc);
-        GBCLab = IGBCLab(_gbcLab);
+    constructor(IERC721 _gbc, GBCLab _lab, address _owner, Authority _authority) Auth(_owner, _authority) {
+        GBC = _gbc;
+        LAB = _lab;
     }
 
     function set(uint gbc, uint[] memory _items, bool[] memory _removes) external {
-        require(GBC.ownerOf(gbc) == msg.sender, "Manager: not owner of gbc");
+        if (GBC.ownerOf(gbc) == msg.sender) revert NotOwner();
         Items storage items = itemsOf[gbc];
         for (uint256 i = 0; i < _items.length; i++) {
             uint item = _items[i];
             bool remove = _removes[i];
             if(!remove) {
-                require(GBCLab.balanceOf(msg.sender, item) > 0, "Manager: Account doesn't own the item");
-                GBCLab.burn(msg.sender, item, 1);
+                LAB.burn(msg.sender, item, 1);
             }
 
             uint itemType = getAttributeOf[item];
 
             if(itemType == BACKGROUND_ATTRIBUTE) {
-                if(items.background != 0) GBCLab.mint(msg.sender, items.background, 1, "");
+                if(items.background != 0) LAB.mint(msg.sender, items.background, 1, "");
                 items.background = remove ? 0 : item;
             } else if(itemType == SPECIAL_ATTRIBUTE) {
-                if(items.special != 0) GBCLab.mint(msg.sender, items.special, 1, "");
+                if(items.special != 0) LAB.mint(msg.sender, items.special, 1, "");
                 items.special = remove ? 0 : item;
             } else {
-                if(items.custom != 0) GBCLab.mint(msg.sender, items.custom, 1, "");
+                if(items.custom != 0) LAB.mint(msg.sender, items.custom, 1, "");
                 items.custom = remove ? 0 : item;
             }
         }
@@ -70,10 +71,7 @@ contract Closet is Auth {
     }
 
     function setItemType(uint id, uint attribute) external requiresAuth {
-        if (getAttributeOf[id] != 0) revert Error_ItemAlreadyExist();
         getAttributeOf[id] = attribute;
     }
 
 }
-
-error Error_ItemAlreadyExist();
